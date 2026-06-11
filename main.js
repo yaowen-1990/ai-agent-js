1dd4fa24d42a315ba5022687eced2393fb3fbb73
yaowen-1990 <59812838+yaowen-1990@users.noreply.github.com>
2026-06-11 09:09:15 +0000

    test

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import { initMessage, addMessage, getMessages } from "./db/messages.js";
 const client = new OpenAI({ apiKey: OPENAI_API_KEY });
 
 await initMessage(
-  "你是一位專門講關於貓的笑話大師，請用繁體中文回答。請用幽默有趣的方式回應。"
+  "你test"
 );
 
 try {
